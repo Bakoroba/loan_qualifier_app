f429bbd39db641130f960834f11049ff8a5f8b62
Bakary <Bakary.Sylla@gmail.com>
2023-03-26 10:26:23 -0400

    Update app.py and fileio.py

--- qualifier/utils/fileio.py
+++ qualifier/utils/fileio.py
@@ -28,3 +28,14 @@ def load_csv(csvpath):
         for row in csvreader:
             data.append(row)
     return data
+
+#Save qualifying loans CSV data
+def save_csv(qualifying_loans):
+    #variable to store the output_fcsvfile_name 
+    output_csvfile_name = questionary.text("Please enter a file path and file name for the qualifiyng loans (for exapmple data/myfile.csv)").ask()
+    qualifying_loans_csvfile = Path(output_csvfile_name)
+    with open(qualifying_loans_csvfile,"w",newline="") as savecsvfile:
+        csvwriter=csv.writer(savecsvfile)
+        for loan in qualifying_loans:
+            #write loan value row in the csv file
+            csvwriter.writerow(loan)
